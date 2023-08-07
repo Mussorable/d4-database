3880244977d44ca5d467e74b7507d5eeeae909fa
Mussorable <redduck5601@gmail.com>
2023-08-07 06:28:11 +0200

    added GET skills router, convert Buffer to base64 string and send it as json format

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import { mongoDB } from "./config.json";
 import skillRouter from "./routes/skills.router";
 
 const startApplication = async () => {
   const app = express();
+  app.use(cors());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
